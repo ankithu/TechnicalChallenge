126a2566de36de7f65ed93ade6605459cac7d884
Ankith Udupa <ankith.udupa@gmail.com>
2021-02-20 14:05:21 -0500

    Initial Commit

---
+++ Stryd Tech ChallengeTests/Stryd_Tech_ChallengeTests.swift
@@ -0,0 +1,33 @@
+//
+//  Stryd_Tech_ChallengeTests.swift
+//  Stryd Tech ChallengeTests
+//
+//  Created by Ankith Udupa on 2/20/21.
+//
+
+import XCTest
+@testable import Stryd_Tech_Challenge
+
+class Stryd_Tech_ChallengeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
